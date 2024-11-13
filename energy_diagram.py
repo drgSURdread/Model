625c1d20af3bef2f279da038ff76bcc83743544e
evgeniy.kondratyev <evgeniy.kondratyev@engee.cloud>
2024-11-13 21:14:15 +0300

    Заготовка функции start

--- energy_diagram.py
+++ energy_diagram.py
@@ -8,3 +8,15 @@ class EnergyDiagram:
         self.channel_name = channel_name
         self.parameter_name = parameter_name
         self.value_lst = value_lst
+
+        self.cycles = dict() # Хранит найденные на текущей итерации ПЦ
+    
+    def start(self, nu_matrix: np.ndarray) -> None:
+        """
+        Перебираем НУ в квадрате со сторонами [angle_min, angle_max] и [velocity_min, velocity_max]
+        """
+        for angle_start in nu_matrix[0]:
+            for velocity_start in nu_matrix[1]:
+                # Для каждого НУ инициализируем решатель и решаем, пока не получим цикл
+                print("Начинаем движение из точки ({}, {})".format(angle_start * 180 / np.pi, velocity_start * 180 / np.pi))
+                
